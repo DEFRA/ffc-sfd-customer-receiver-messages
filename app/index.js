e25244bbd4af946258fa576405596f44ceab7595
Rana Salem <ranatasalem@gmail.com>
2024-04-30 13:50:40 +0100

    persist ASB messages in CosmosDB + update docker network & start script

--- app/index.js
+++ app/index.js
@@ -1,10 +1,18 @@
 require('./insights').setup()
 require('log-timestamp')
 const { startMessaging } = require('./messaging')
+const { DEVELOPMENT } = require('./constants/environments')
+// const { saveToCosmos } = require('./messaging/save-to-cosmos')
+
+// Disable TLS validation in development to allow connection to cosmosDb emulator
+if (process.env.NODE_ENV === DEVELOPMENT) {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+}
 
 const init = async () => {
   await startMessaging()
-  console.log('Running app')
+  // await saveToCosmos({ id: '2', body: 'Hello' })
+  console.log('Running receiver service for messages')
 }
 
 process.on('unhandledRejection', (err) => {
--- app/messaging/index.js
+++ app/messaging/index.js
@@ -1,10 +1,12 @@
 const util = require('util')
 const { messageConfig } = require('../config')
 const { MessageReceiver } = require('ffc-messaging')
+const { saveToCosmos } = require('./save-to-cosmos')
 
 const handleMessage = async (message, receiver) => {
   try {
     console.log('Received message:', message.body)
+    await saveToCosmos(message)
     await receiver.completeMessage(message)
   } catch (err) {
     console.error('Message error', util.inspect(err.message, false, null, true))
@@ -13,13 +15,13 @@ const handleMessage = async (message, receiver) => {
 
 const startMessaging = async () => {
   let messagesReceiver //eslint-disable-line
-  const receiverAction = message => handleMessage(message, messagesReceiver)
+  const receiverAction = (message) => handleMessage(message, messagesReceiver)
   messagesReceiver = new MessageReceiver(
     messageConfig.receiverSubscription,
     receiverAction
   )
   await messagesReceiver.subscribe()
-  console.info('Messages receiver is ready to receive messages')
+  console.info('Messages receiver is ready to consume messages')
 }
 
 module.exports = { startMessaging }
