da0a91b13ef7df0f4843b2d0aec31de6bbdd4793
Rana Salem <ranatasalem@gmail.com>
2024-04-30 14:06:00 +0100

    remove comments

--- app/index.js
+++ app/index.js
@@ -2,7 +2,6 @@ require('./insights').setup()
 require('log-timestamp')
 const { startMessaging } = require('./messaging')
 const { DEVELOPMENT } = require('./constants/environments')
-// const { saveToCosmos } = require('./messaging/save-to-cosmos')
 
 // Disable TLS validation in development to allow connection to cosmosDb emulator
 if (process.env.NODE_ENV === DEVELOPMENT) {
@@ -11,7 +10,6 @@ if (process.env.NODE_ENV === DEVELOPMENT) {
 
 const init = async () => {
   await startMessaging()
-  // await saveToCosmos({ id: '2', body: 'Hello' })
   console.log('Running receiver service for messages')
 }
 
