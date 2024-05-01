767fb221e8f57f62ef1fd35117991bfd5f508398
Rana Salem <ranatasalem@gmail.com>
2024-05-01 14:51:12 +0100

    add updates based on PR review

--- app/messages/save-to-cosmos.js
+++ app/messages/save-to-cosmos.js
@@ -5,15 +5,11 @@ const saveToCosmos = async (message) => {
   try {
     const { messagesDatabase } = await cosmos()
 
-    const item = {
-      body: message.body
-    }
-
     await messagesDatabase
       .container(cosmosConfig.messagesContainer)
-      .items.create(item)
+      .items.create(message.body)
 
-    console.log('Message saved to CosmosDB:', item)
+    console.log('Message saved to CosmosDB:', message.body)
   } catch (error) {
     console.error('Error saving message to Cosmos:', error)
   }
